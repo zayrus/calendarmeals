24b0c47a37bb796c3ea3f953ffa921d8c2b7b264
Ernest <zayrus@gmail.com>
2017-12-05 23:40:50 -0300

    refactor(whole project): replace redux low level with react-redux provider

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 import { createStore } from 'redux'
 import reducer from './reducers'
+import { Provider } from 'react-redux'
 
 //create store from redux and pass created reducer
 const store = createStore(
@@ -12,5 +13,7 @@ const store = createStore(
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 )
 
-ReactDOM.render(<App store={ store }/>, document.getElementById('root'));
+ReactDOM.render(<Provider  store={ store }>
+    <App/>
+</Provider>, document.getElementById('root'));
 registerServiceWorker();
