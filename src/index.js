229f18568b9eb81d619c128f914edbc19717d2e0
Ernesto F Venencio <zayrus@gmail.com>
2017-12-12 17:58:38 -0300

    feat(add api call and all functionality+):

--- src/index.js
+++ src/index.js
@@ -3,17 +3,34 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
-import { createStore } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import reducer from './reducers'
 import { Provider } from 'react-redux'
+
+const logger = store => next => action => {
+  console.group(action.type)
+  console.info('dispatching', action)
+  let result = next(action)
+  console.log('next state', store.getState())
+  console.groupEnd(action.type)
+  return result
+}
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
 //create store from redux and pass created reducer
 const store = createStore(
     reducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  composeEnhancers(
+    applyMiddleware(logger)
+  )
 )
 
-ReactDOM.render(<Provider  store={ store }>
+ReactDOM.render(
+  <Provider  store={ store }>
     <App/>
-</Provider>, document.getElementById('root'));
-registerServiceWorker();
+  </Provider>,
+  document.getElementById('root')
+)
+registerServiceWorker()
+
